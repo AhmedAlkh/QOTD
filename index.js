703579f50b006e6c9f5d71ac2860714dfd327423
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-05-07 11:20:32 -0400

    created array, added quotes

---
+++ index.js
@@ -0,0 +1,14 @@
+var arrayOfQuotes = [
+    {
+        "author":"Jim Rohn",
+        "quote":"Beware of what you become in pursuit of what you want."
+    },
+    {
+        "author":"Epictetus",
+        "quote":"It's not what happens to you, but how you react to it that matters."
+    },
+    {
+        "author":"Frank Sinatra",
+        "quote":"The best revenge is massive success."
+    }
+]
